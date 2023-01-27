ff8078c236b3185a65aa8dc299088bf6f28d0f22
eric <collinseric321@gmail.com>
2023-01-26 17:47:21 -0700

    update

--- constants.py
+++ constants.py
@@ -1,12 +1,13 @@
 import itertools
 
-user_fields_needed = ['unique_id']
-video_fields_needed = ['id', 'stats.digg_count', 
-                       'stats.share_count', 
-                       'stats.comment_count', 
-                       'stats.play_count'
-                       'create_time']
-tags_fields_needed = ['title']
-all_fields_needed = itertools.chain(user_fields_needed, 
-                             video_fields_needed, 
-                             tags_fields_needed)
+# The keys are the column names in the output file,
+# the values are the attribute name in the API return object
+user_fields = {'user_unique_id': 'unique_id'}
+video_fields = {'video_id': 'id', 
+                'video_create_time': 'create_time'}
+video_stats_fields = {'video_digg_count': 'digg_count', 
+                      'video_share_count': 'share_count', 
+                      'video_comment_count': 'comment_count', 
+                      'video_play_count': 'play_count',}
+tags_fields = {'hashtag': 'title'}
+all_fields_needed = {**user_fields, **video_fields, **video_stats_fields, **tags_fields}
---
+++ googledrive/googledrive.py
@@ -0,0 +1,3 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+gauth = GoogleAuth()           
